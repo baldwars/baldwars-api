a686a4db91cedcf3e38a2bfcb70c0a1fd250a3a9
Antonin Texier <antonin.texier@gmail.com>
2021-05-17 20:04:26 +0200

    feat(godbox): Add class for data coming from godbox service.

---
+++ src/main/java/fr/esgi/baldwarsapi/exposition/godbox/GodBoxResponse.java
@@ -0,0 +1,32 @@
+package fr.esgi.baldwarsapi.exposition.godbox;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class GodBoxResponse {
+
+    @JsonProperty("compile_status")
+    private Integer compileStatus;
+
+    @JsonProperty("compile_stdout")
+    private String compileStdOut;
+
+    @JsonProperty("compile_stderr")
+    private String compileStdErr;
+
+    @JsonProperty("run_status")
+    private Integer runStatus;
+
+    @JsonProperty("run_stdout")
+    private String runStdOut;
+
+    @JsonProperty("run_stderr")
+    private String runStdErr;
+
+    @JsonProperty("profiling_result")
+    private String profilingResult;
+}

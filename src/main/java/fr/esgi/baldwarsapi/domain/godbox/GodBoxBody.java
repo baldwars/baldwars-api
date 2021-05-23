4d71453de8d9bb7e1a18001a12a2cd10e465c4d2
Antonin Texier <antonin.texier@gmail.com>
2021-05-23 11:59:33 +0200

    debug for prod

--- src/main/java/fr/esgi/baldwarsapi/domain/godbox/GodBoxBody.java
+++ src/main/java/fr/esgi/baldwarsapi/domain/godbox/GodBoxBody.java
@@ -18,6 +18,7 @@ public class GodBoxBody {
     public GodBoxBody(String username, String filesBase64) {
         this.compileScript = "/usr/local/gcc-11.1.0/bin/gcc " + username + "/main.c -o out";
         this.runScript = "./out";
-        this.filesBase64 = filesBase64;
+//        this.filesBase64 = filesBase64;
+        this.filesBase64 = "UEsDBBQAAAgIAMYOt1IfZKBYRAAAAEQAAAARAAAAY2h1a2l0aXBvay9tYWluLmNTzsxLzilNSVWwKS5JyczXy7Dj4srMK1HITczM09BUqOYqKAJy0zSUPFJzcvIVwvOLclIUlTStuYpSS0qL8hQMrLlqAVBLAwQKAAAIAADGDrdSAAAAAAAAAAAAAAAACwAAAGNodWtpdGlwb2svUEsBAjMDFAAACAgAxg63Uh9koFhEAAAARAAAABEAAAAAAAAAAAAAAKSBAAAAAGNodWtpdGlwb2svbWFpbi5jUEsBAjMDCgAACAAAxg63UgAAAAAAAAAAAAAAAAsAAAAAAAAAAAAAAO1BcwAAAGNodWtpdGlwb2svUEsFBgAAAAACAAIAeAAAAJwAAAAAAA==";
     }
 }

acb191ead8b9fd17a7de3d2b150ea69de74c3464
Antonin Texier <antonin.texier@gmail.com>
2021-05-27 21:36:18 +0200

    config(cors): Try to resolve CORS policy issue on prod.

--- src/main/java/fr/esgi/baldwarsapi/security/WebConfig.java
+++ src/main/java/fr/esgi/baldwarsapi/security/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://baldwars.chukitipok.qtmsheep.com")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(false);

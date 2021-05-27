83521f98b0b3d61fb4fde785caf6967911a7eee8
Antonin Texier <antonin.texier@gmail.com>
2021-05-27 19:56:58 +0200

    config(cors): Try to resolve CORS policy issue on prod.

--- src/main/java/fr/esgi/baldwarsapi/BaldWarsApiApplication.java
+++ src/main/java/fr/esgi/baldwarsapi/BaldWarsApiApplication.java
@@ -3,6 +3,10 @@ package fr.esgi.baldwarsapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BaldWarsApiApplication extends SpringBootServletInitializer {
@@ -11,4 +15,13 @@ public class BaldWarsApiApplication extends SpringBootServletInitializer {
         SpringApplication.run(BaldWarsApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }

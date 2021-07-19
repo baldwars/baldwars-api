c108ef7b7d6489620dcebd7c597e5bca75888370
chukitipok <antonin.texier@gmail.com>
2021-07-20 01:28:59 +0200

    feat(plagiarism): Create script model.

---
+++ src/main/java/fr/esgi/baldwarsapi/domain/scripts/models/Script.java
@@ -0,0 +1,18 @@
+package fr.esgi.baldwarsapi.domain.scripts.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Script {
+
+    private UUID id;
+    private UUID owner;
+    private String content;
+
+}

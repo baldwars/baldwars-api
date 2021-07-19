22ca69d94f55fd02d7e00da9710c8c195b4c4cfd
chukitipok <antonin.texier@gmail.com>
2021-07-20 00:02:49 +0200

    feat(antlr): Implement visitors to generify variables before plagiarism analysis.

---
+++ src/main/java/fr/esgi/baldwarsapi/domain/analysis/plagiarism/TransformerVisitor.java
@@ -0,0 +1,48 @@
+package fr.esgi.baldwarsapi.domain.analysis.plagiarism;
+
+import fr.esgi.baldwarsapi.antlr4.CLexer;
+import fr.esgi.baldwarsapi.antlr4.CParser;
+import lombok.SneakyThrows;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.antlr.v4.runtime.CharStreams.fromFileName;
+
+public class TransformerVisitor {
+
+    private final VariableTransformerVisitor variableVisitor;
+    private final ParserRuleContext context;
+
+    @SneakyThrows
+    public TransformerVisitor(String source) {
+        try {
+            var inputStream = fromFileName(source);
+            var lexer = new CLexer(inputStream);
+            var tokenStream = new CommonTokenStream(lexer);
+            var parser = new CParser(tokenStream);
+            context = parser.blockItem();
+
+            var path = Paths.get(source);
+            var content = Files.readAllLines(path);
+
+            variableVisitor = new VariableTransformerVisitor(content);
+
+        } catch (IOException e) {
+            throw new FileNotFoundException();
+        }
+    }
+
+    public List<String> visit() {
+        variableVisitor.visit(context);
+        var argumentVisitor = new ArgumentTransformerVisitor(variableVisitor.getSource());
+        argumentVisitor.visit(context);
+
+        return argumentVisitor.getSource();
+    }
+}

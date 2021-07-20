44e74ee6e3d1c7a2beccccaad0c41aac2fdc1eec
chukitipok <antonin.texier@gmail.com>
2021-07-20 16:44:25 +0200

    feat(plagiarism): Compare user script to all other users scripts.

--- src/main/java/fr/esgi/baldwarsapi/domain/analysis/plagiarism/PlagiarismService.java
+++ src/main/java/fr/esgi/baldwarsapi/domain/analysis/plagiarism/PlagiarismService.java
@@ -1,18 +1,40 @@
 package fr.esgi.baldwarsapi.domain.analysis.plagiarism;
 
+import fr.esgi.baldwarsapi.domain.analysis.plagiarism.visitors.TransformerVisitor;
+import fr.esgi.baldwarsapi.domain.scripts.ScriptService;
+import fr.esgi.baldwarsapi.domain.scripts.models.Script;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PlagiarismService {
 
+    private final ScriptService scriptService;
     private final PlagiarismSimilarity similarityService;
 
-    public void analyse(UUID owner, String source) {
+    public List<Script> analyse(Script source) {
+        var scripts = this.scriptService.findAll();
+        var userScripts = this.scriptService.findAllUserScripts(source.getOwner());
 
+        scripts.removeAll(userScripts);
+
+        var sourceContent = new TransformerVisitor(source.getContent()).visit();
+        var similarScripts = new ArrayList<Script>();
+
+        for (var script : scripts) {
+            var scriptContent = new TransformerVisitor(script.getContent()).visit();
+            var similarity = this.similarityService.winnowing(sourceContent, scriptContent);
+
+            if (isCheater(similarity)) {
+                similarScripts.add(script);
+            }
+        }
+
+        return similarScripts;
     }
 
     public static boolean isCheater(Double similarity) {

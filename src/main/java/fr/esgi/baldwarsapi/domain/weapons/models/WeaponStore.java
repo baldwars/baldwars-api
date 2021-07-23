a9fd60a8b15ce72305de30fb3b5f74e17b2853df
chukitipok <antonin.texier@gmail.com>
2021-07-23 17:45:44 +0200

    feat(weapons): Create weapon models.

---
+++ src/main/java/fr/esgi/baldwarsapi/domain/weapons/models/WeaponStore.java
@@ -0,0 +1,21 @@
+package fr.esgi.baldwarsapi.domain.weapons.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeaponStore {
+
+    private Integer id;
+    private String name;
+    private Integer level;
+    private Integer damage;
+    private Integer cost;
+    private Integer minRange;
+    private Integer maxRange;
+    private Integer price;
+
+}
